d91d8824a063e0a60b383eb689feabae0cb73af9
bscoderuz <mrbscoder@gmail.com>
2023-01-26 12:34:25 -0800

    debug = True

--- scholl_/settings.py
+++ scholl_/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1t6gvbg9@g9lopd@h0mwwl*3m8o1alqmzc@u^9niisniuz+fwc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
