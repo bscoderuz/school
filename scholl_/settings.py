2912ee9c27dbcc6d924f45a660b8f3a0b5d12be4
bscoderuz <mrbscoder@gmail.com>
2023-01-28 02:11:30 -0800

    add

--- scholl_/settings.py
+++ scholl_/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1t6gvbg9@g9lopd@h0mwwl*3m8o1alqmzc@u^9niisniuz+fwc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["www.rbot.elitemedcentre.uz", 'rbot.elitemedcentre.uz']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
