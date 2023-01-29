7b7f042ad97dadcfc862b92bc02df8f7df7ccce3
bscoderuz <mrbscoder@gmail.com>
2023-01-29 05:36:34 -0800

    add register views

--- basic/urls.py
+++ basic/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import SkillsViewSet, CourseViewSet, add_register_course, get_register_course
+from .views import SkillsViewSet, CourseViewSet, RegisterCourseViewSet, get_register_course
 
 router = routers.DefaultRouter()
 
@@ -11,7 +11,8 @@ router.register('course', CourseViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('register_course/', add_register_course),
+    # path('register_course/', add_register_course),
     path('get_register_course/', get_register_course),
+    path('register_course/', RegisterCourseViewSet.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- basic/views.py
+++ basic/views.py
@@ -1,15 +1,12 @@
-from rest_framework import serializers, viewsets, status
+from rest_framework import serializers, viewsets, status, generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .serializers import SkillsSerializer, CourseSerializer, RegistrationCourseSerializer
 from .models import Skills, Course, RegistrationCourse
 
-
 # Create your views here.
-
 
 class SkillsViewSet(viewsets.ModelViewSet):
     queryset = Skills.objects.all()
@@ -20,16 +17,6 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    # class RegistrationCourseViewSet(viewsets.ModelViewSet):
-    #     # permission_classes = [IsAuthenticated]
-    #     queryset = RegistrationCourse.objects.all()
-    #     serializer_class = RegistrationCourseSerializer
-    #
-    #     def get(self, request, format=None):
-    #         snippets = Snippet.objects.all()
-    #         serializer = SnippetSerializer(snippets, many=True)
-    #         return Response(serializer.data)
-
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -39,12 +26,16 @@ def get_register_course(self):
     return Response(serializer.data)
 
 
-@api_view(['POST'])
-def add_register_course(request):
+# @api_view(['POST'])
+# def add_register_course(request):
+#     serializer = RegistrationCourseSerializer(data=request.data)
+#
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    serializer = RegistrationCourseSerializer(data=request.data)
 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class RegisterCourseViewSet(generics.ListCreateAPIView):
+    queryset = RegistrationCourse.objects.all()
+    serializer_class = RegistrationCourseSerializer
